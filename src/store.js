77e1d4b5cd95e92d31efd8de646c1b81d8b4a4cd
Trevor Jacklitch <trevorjacklitch123@gmail.com>
2018-11-26 19:23:51 -0600

    Cleaning up code

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers/index.js';
+import { formSubmitted, rightCardClicked, wrongCardClicked } from './actions/actions';
 
 const initialState = {};
 
@@ -8,5 +9,9 @@ const store = createStore(
     initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(formSubmitted(["Spades"]));
 
 export default store;
